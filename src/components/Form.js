9b68a96dafacb394bcc5006fa09dcfe42aef6fc6
leohon <leo.cl.hon@gmail.com>
2023-06-02 12:47:42 -0400

    Functioning form before Firestore aspect.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,40 @@
 export default function Form() {
+  const check = (letter) => {
+    letter = letter.toUpperCase();
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    for (let i = 0; i < alphabet.length; i++) {
+      if (letter === alphabet[i]) {
+        console.log("true" + i);
+      }
+    }
+  }
+  
+  const clear = (e) => {
+    e.preventDefault();
+
+    const form = document.querySelector("form");
+    const name = document.getElementById("name");
+
+    check(name.value[0]);
+    form.reset();
+  }
+
   return (
     <section className="form">
-      <form action="">
+      <form onSubmit={clear}>
         <h2>Restaurant</h2>
 
-        <label htmlFor="">Name</label>
-        <input type="text" />
+        <label htmlFor="name">Name</label>
+        <input type="text" className="name" id="name" required/>
         
-        <label htmlFor="">Address</label>
-        <input type="text" />
+        <label htmlFor="address">Address</label>
+        <input type="text" className="address" id="address" /> {/* required */}
         
-        <label htmlFor="">Recommended Dish</label>
-        <input type="text" />
+        <label htmlFor="dish">Recommended Dish</label>
+        <input type="text" className="dish" id="dish" /> {/* required */}
 
-        <button>Submit</button>
+        <button className="submit">Submit</button>
       </form>
     </section>
   )
