b05ca34972d8a8a86dc3b34bbf0f9194ac4ea813
leohon <leo.cl.hon@gmail.com>
2023-06-01 23:19:05 -0400

    Added content for Steps & Form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,20 @@
 export default function Form() {
   return (
     <div>
-      <form action=""></form>
+      <form action="">
+        <h2>Restaurant</h2>
+
+        <label htmlFor="">Name</label>
+        <input type="text" />
+        
+        <label htmlFor="">Address</label>
+        <input type="text" />
+        
+        <label htmlFor="">Recommended Dish</label>
+        <input type="text" />
+
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -2,7 +2,7 @@ export default function Steps() {
   return (
     <div>
       <h2>Instructions</h2>
-      <p></p>
+      <p>Complete the form by inputting the restaurant name, address, and a recommended/favourite dish. Submit the form to add your restaurant to the address book below.</p>
     </div>
   )
 }
