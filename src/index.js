31b3ff3de4a48b85a06571afaf2dcaf19b750a42
leohon <leo.cl.hon@gmail.com>
2023-06-03 16:38:17 -0400

    Commented out code causing Netlify build error.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 import { FirebaseAppProvider } from 'reactfire';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
