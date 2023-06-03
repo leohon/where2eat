5a851fae5f45100f51b9b2052a6b686f8f745bde
leohon <leo.cl.hon@gmail.com>
2023-06-03 12:18:58 -0400

    added a subtitle div

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ export default function Header() {
     <header>
       <div className="wrapper">
         <h1>Where 2 Eat @ ?</h1>
-        <h4>Can't remember the name or area of the restaurant to choose where to eat at?</h4>
-        <h4>Store your favourite places here for your next 'eating out' day!</h4>
+        <div className="subtitle">
+          <h4>Can't remember the name or area of the restaurant to choose where to eat at?</h4>
+          <h4>Store your favourite places here for your next 'eating out' day!</h4>
+        </div>
       </div>
     </header>
   )
