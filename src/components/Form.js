65a99cbe07233f745fd098d007a4e82353442554
leohon <leo.cl.hon@gmail.com>
2023-06-09 23:06:44 -0400

    Removed unneeded code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,7 @@
-export default function Form() {
-  const check = (letter) => {
-    letter = letter.toUpperCase();
-    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-    for (let i = 0; i < alphabet.length; i++) {
-      if (letter === alphabet[i]) {
-        console.log("true" + i);
-      }
-    }
-  }
-  
+export default function Form() {  
   const clear = (e) => {
     e.preventDefault();
-
-    const form = document.querySelector("form");
-    // const name = document.getElementById("name");
-    // const address = document.getElementById("address");
-    // const dish = document.getElementById("dish");
-
-    // check(name.value[0]);
-    form.reset();
+    document.querySelector("form").reset();
   }
 
   return (
